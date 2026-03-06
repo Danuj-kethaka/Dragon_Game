d6cac95144fa16251c9b6bd70b249ab647194325
Danuj-kethaka <senanayakakethaka@gmail.com>
2026-03-06 20:33:26 +0530

    Added destroy animation for objects

---
+++ Assets/Scripts/Weapons/Fire.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float speed = 10f;
+    [SerializeField] private GameObject destroyEffect;
+
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+    void Start()
+    {
+        Destroy (gameObject,3f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Obstacle") || collision.CompareTag("Whale"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            Instantiate(destroyEffect,transform.position, transform.rotation);
+        }
+    }
+}
